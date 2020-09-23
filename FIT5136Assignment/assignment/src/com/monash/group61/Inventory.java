384fa833acdaf06aa3707cadcfe61cec59280438
Fanwen Meng <mengfanwen05@gmail.com>
2020-09-23 19:49:52 +1000

    finish entity class of inventory and report

--- FIT5136Assignment/assignment/src/com/monash/group61/Inventory.java
+++ FIT5136Assignment/assignment/src/com/monash/group61/Inventory.java
@@ -1,5 +1,51 @@
 package com.monash.group61;
 
+
 public class Inventory {
-    private int var;
+    private int itemId;
+    private String itemName;
+    private int itemQuantityInStock;
+    private String dateInventoryAdded;
+
+    public Inventory() {
+    }
+
+    public Inventory(int itemId, String itemName, int itemQuantityInStock, String dateInventoryAdded) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemQuantityInStock = itemQuantityInStock;
+        this.dateInventoryAdded = dateInventoryAdded;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemQuantityInStock() {
+        return itemQuantityInStock;
+    }
+
+    public void setItemQuantityInStock(int itemQuantityInStock) {
+        this.itemQuantityInStock = itemQuantityInStock;
+    }
+
+    public String getDateInventoryAdded() {
+        return dateInventoryAdded;
+    }
+
+    public void setDateInventoryAdded(String dateInventoryAdded) {
+        this.dateInventoryAdded = dateInventoryAdded;
+    }
 }
--- FIT5136Assignment/assignment/src/com/monash/group61/Report.java
+++ FIT5136Assignment/assignment/src/com/monash/group61/Report.java
@@ -1,5 +1,32 @@
 package com.monash.group61;
 
+import java.util.Date;
+
 public class Report {
-    int i =0;
+    private String type;
+    private Date timePeriod;
+
+    public Report() {
+    }
+
+    public Report(String type, Date timePeriod) {
+        this.type = type;
+        this.timePeriod = timePeriod;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getTimePeriod() {
+        return timePeriod;
+    }
+
+    public void setTimePeriod(Date timePeriod) {
+        this.timePeriod = timePeriod;
+    }
 }
